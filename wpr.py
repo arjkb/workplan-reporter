802308fd32e91e3955b9f53f16a49bb4994fb60a
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2024-10-14 22:40:28 +0530

    read the assignee names from the config file

--- wpr.py
+++ wpr.py
@@ -11,10 +11,8 @@ import sys
 from status import status
 from workplan import entry, workplan
 
-ASSIGNEE_NAME_POSSIBILITIES = ['Example Name', 'Example',]
-
 # Generate WorkPlan from the csv file.
-def generate_workplan_from_csv(csvfilename: str) -> workplan.WorkPlan:
+def generate_workplan_from_csv(csvfilename: str, assignees: list[str]) -> workplan.WorkPlan:
     curr_header = ''
     wp = workplan.WorkPlan()
     with open(csvfilename, 'r', newline='') as csvfile:
@@ -26,7 +24,7 @@ def generate_workplan_from_csv(csvfilename: str) -> workplan.WorkPlan:
                     curr_header = row[0]
                 else:
                     req_name, stat, ecd, assignee = row[0], row[4], row[9], row[10]
-                    if assignee in ASSIGNEE_NAME_POSSIBILITIES:
+                    if assignee in assignees:
                         wp.add_entry(entry.Entry(curr_header, req_name, status.Status[stat.upper()], ecd, assignee))
     return wp
 
@@ -39,26 +37,27 @@ def main():
     prev_week_fname = f"inputs/sprint_{(today - datetime.timedelta(weeks=1)).strftime(DATE_FORMAT)}.csv"
     output_fname = f"outputs/sprint_report_{today_date_suffix}.csv"
 
-    # read URL from config
-    with open('url.txt', 'r') as f:
-        url = f.readline()
+    with open('config.txt', 'r') as f:
+        lines = f.readlines()
+        url = lines[0].strip()
+        assignee_names = [assignee_name.strip() for assignee_name in lines[1].strip().split(',')]
 
     # fetch the file from the URL and save it to the appropriate location
     open(curr_week_fname, 'wb').write(requests.get(url).content)
 
     # make the workplan entries of current and previous weeks
-    curr_wp = generate_workplan_from_csv(curr_week_fname) if os.path.isfile(curr_week_fname) else None
+    curr_wp = generate_workplan_from_csv(curr_week_fname, assignee_names) if os.path.isfile(curr_week_fname) else None
     if curr_wp is None:
         print("failed to generate current week's file")
         sys.exit(-1)
 
     if os.path.isfile(prev_week_fname):
-        result_wp = curr_wp - generate_workplan_from_csv(prev_week_fname)
+        result_wp = curr_wp - generate_workplan_from_csv(prev_week_fname, assignee_names)
     else:
         result_wp = curr_wp
 
     output_text = (
-        f"Weekly report for the week ending on {date_string}"
+        f"Weekly report for the week ending on {date_string}."
         + str(result_wp)
         + "\n\nIn the unlikely event of a mismatch between Jira and the workplan, prefer entry in the latter."
     )
