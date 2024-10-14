ebb5a8d52d74815f1225c4f9d3b4d7b90bd17ef6
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2024-10-14 22:07:48 +0530

    print and save the computed result

--- wpr.py
+++ wpr.py
@@ -3,7 +3,9 @@
 
 import csv
 import datetime
+import os
 import requests
+import sys
 
 from status import status
 from workplan import entry, workplan
@@ -31,27 +33,38 @@ def main():
     # read URL from config
     with open('url.txt', 'r') as f:
         url = f.readline()
-    print(url)
 
     # determine filenames
     DATE_FORMAT = '%Y%m%d'
     today = datetime.date.today()
     today_date_suffix = today.strftime(DATE_FORMAT)
+    date_string = today.strftime("%a %-d %b %Y")
     curr_week_fname = f"inputs/sprint_{today_date_suffix}.csv"
     prev_week_fname = f"inputs/sprint_{(today - datetime.timedelta(weeks=1)).strftime(DATE_FORMAT)}.csv"
+    output_fname = f"outputs/sprint_report_{today_date_suffix}.csv"
 
     # fetch the file from the URL and save it to the appropriate location
     open(curr_week_fname, 'wb').write(requests.get(url).content)
 
-    # make the workplan entry of that file
-    curr_wp = generate_workplan_from_csv(curr_week_fname)
-    print(curr_wp)
+    # make the workplan entries of current and previous weeks
+    curr_wp = generate_workplan_from_csv(curr_week_fname) if os.path.isfile(curr_week_fname) else None
+    if curr_wp is None:
+        print("failed to generate current week's file")
+        sys.exit(-1)
 
-    # TODO: read last week's entry if available and subtract that from this week's entry
+    if os.path.isfile(prev_week_fname):
+        result_wp = curr_wp - generate_workplan_from_csv(prev_week_fname)
+    else:
+        result_wp = curr_wp
 
-    # TODO: print the output
+    output_text = (
+        f"Weekly report for the week ending on {date_string}"
+        + str(result_wp)
+        + "\n\nIn the unlikely event of a mismatch between Jira and the workplan, prefer entry in the latter."
+    )
 
-    # TODO: save the output to the clipboard
+    print(output_text)
+    open(output_fname, 'w').write(output_text)
 
 if __name__ == '__main__':
     main()
